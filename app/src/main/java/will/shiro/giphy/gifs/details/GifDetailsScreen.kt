80fb3d4406bc9f52767c698a1e594c02139ceaa4
William Kanashiro <wyskanashiro@gmail.com>
2024-03-05 17:34:33 -0400

    String resource

--- app/src/main/java/will/shiro/giphy/gifs/details/GifDetailsScreen.kt
+++ app/src/main/java/will/shiro/giphy/gifs/details/GifDetailsScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -31,8 +32,13 @@ internal fun GifDetailsScreen(
         Column {
             Image(
                 painter = painterResource(id = R.drawable.ic_arrow_back_24),
-                contentDescription = "Back button",
-                modifier = Modifier.padding(start = 16.dp, top = 16.dp).clickable { onBackClick() }
+                contentDescription = stringResource(id = R.string.back_button),
+                modifier = Modifier
+                    .padding(
+                        start = 16.dp,
+                        top = 16.dp
+                    )
+                    .clickable { onBackClick() }
             )
             GifDetailsComposable(gifSelected)
         }
